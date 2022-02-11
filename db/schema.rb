220d5d879f4d02c5eb429a35fe8a451fd1d2802e
Igor Silva <igorsillva@hotmail.com.br>
2022-02-11 14:38:20 -0300

    MVC funcionando

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_11_113050) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_11_162607) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
