d218a2f8e3e6eb9a9f29a352c395b7c9330f708d
Igor Silva <igorsillva@hotmail.com.br>
2022-02-14 10:10:06 -0300

    Validações + CRUD

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+
+  #valida o titulo
+  validates :title, presence: true
+  #valida o body, tornando ele obrigatório e com no mínimo 10 caracteres
+  validates :body, presence: true, length: { minimum: 10 }
 end
